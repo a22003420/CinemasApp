90e26418d9d935c3780b77084ec3e6a0242cd747
a22202497 <jpcamatos31@gmail.com>
2023-04-12 17:44:28 +0100

    +BottomBarNavWithIcones+layoutChanges

--- app/src/main/java/View/BottomNavigationActivity.kt
+++ app/src/main/java/View/BottomNavigationActivity.kt
@@ -2,11 +2,37 @@ package View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.cinemas_app.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BottomNavigationActivity : AppCompatActivity() {
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_navigation)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
+        bottomNav.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_dashboard -> {
+                    Toast.makeText(this, "Dashboard", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.navigation_movie_presentation -> {
+                    Toast.makeText(this, "Cinemas", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.navigation_movie_register -> {
+                    Toast.makeText(this, "Registo", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
+
 }
