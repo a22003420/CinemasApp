100edd7cd3c61b767202f3c9372036ece4d02914
a22000492 <ricardo_a_goncalves@hotmail.com>
2023-04-22 01:45:10 +0100

    feat: added dialog for future voice search

--- app/src/main/java/pt/ulusofona/cinemas_app/controller/MainActivity.kt
+++ app/src/main/java/pt/ulusofona/cinemas_app/controller/MainActivity.kt
@@ -1,8 +1,11 @@
 package pt.ulusofona.cinemas_app.controller
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.MenuItem
+import android.widget.TextView
 import com.example.cinemas_app.R
 import com.example.cinemas_app.databinding.ActivityMainBinding
 
@@ -36,8 +39,35 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager
       )
       R.id.navigation_movie_register -> NavigationManager.goToRegistoFragment(supportFragmentManager)
+      R.id.navigation_microphone -> {
+        val countdownDialog: Dialog = createCountdownDialog();
+        countdownDialog.show();
+
+      }
     }
     return true
+  }
 
+  private fun createCountdownDialog(): Dialog {
+    val dialog = Dialog(this)
+    dialog.setContentView(R.layout.voice_search_dialog)
+
+    val countdownText = dialog.findViewById<TextView>(R.id.countdown_text)
+    countdownText.text = "10"
+
+    val handler = Handler()
+    handler.postDelayed(object : Runnable {
+      override fun run() {
+        var countdown = countdownText.text.toString().toInt()
+        countdown--
+        if (countdown == 0) {
+          dialog.dismiss()
+        } else {
+          countdownText.text = countdown.toString()
+          handler.postDelayed(this, 1000)
+        }
+      }
+    }, 1000)
+    return dialog
   }
 }
